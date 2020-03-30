ef0eaa6d62c21d0307bdd9fb723a5608fd9be503
Jonathan Spratte <jspratte@yahoo.de>
2020-03-30 11:09:02 +0200

    added data, dataT, oinitial, einitial

--- build.lua
+++ build.lua
@@ -1,7 +1,7 @@
 -- Build script for expkv-def
 module     = "expkv-def"
-pkgversion = "0.1a"
-pkgdate    = "2020-02-29"
+pkgversion = "0.2"
+pkgdate    = "2020-03-30"
 
 -- update package date and version
 tagfiles = {"expkv-def.dtx", "README.md", "CTAN.md"}
